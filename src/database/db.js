1fd51813c2c7ae81c866e8827071dcdf91917cfb
Caio Beneton <caio.beneton@gmail.com>
2022-11-22 22:57:39 -0300

    Feat: add folders, database and user model

---
+++ src/database/db.js
@@ -0,0 +1,18 @@
+import { MongoClient } from "mongodb"
+import dotenv from "dotenv"
+dotenv.config()
+
+const mongoClient = new MongoClient(process.env.MONGO_URI)
+
+try {
+    await mongoClient.connect()
+    console.log("MongoDB connected!")
+} catch (error) {
+    console.log(error)
+}
+
+const db = mongoClient.db("getMusic")
+export const usersCollection = db.collection("users")
+export const sessionsCollection = db.collection("sessions")
+export const productsCollection = db.collection("products")
+export const cuponsCollection = db.collection("cupons")
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import express from "express"
 import cors from "cors"
+import dotenv from "dotenv"
 
 const app = express()
-
+dotenv.config()
 app.use(cors())
 app.use(json())
 

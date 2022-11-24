b34d9172d80a5011f9a9e49fb270d024d28ba1ba
Igor Guimarães <igorgui.tec@gmail.com>
2022-11-24 15:24:53 -0300

    transaction controller are created

---
+++ src/controllers/transaction.controller.js
@@ -0,0 +1,26 @@
+import { productsCollection, transactionsCollection, usersCollection } from "../database/db";
+
+export async function purcharse(req, res) {
+    const transaction = req.body
+    const { email, productId } = transaction
+    try {
+        const product = await productsCollection.findOne({ productId })
+        const user = await usersCollection.findOne({ email })
+
+        const transactionToPost = {
+            ...transaction,
+            clientId: user._id,
+            productId: product,_id,
+            productName: product.name
+        }
+        
+        await transactionsCollection.insertOne(transactionToPost)
+
+
+        res.sendStatus(201)
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+
+}

2b976868c16045dd7a85d411e93a6161022c7adf
Igor Guimarães <igorgui.tec@gmail.com>
2022-11-24 13:06:28 -0300

    feat: joi Transação

---
+++ src/models/transaction.model.js
@@ -0,0 +1,13 @@
+import joi from 'joi'
+
+const transactionSchema = joi.object({
+    userName: joi.string().required().min(3),
+    email: joi.string().email().required().min(8),
+    cardData: joi.object({
+        front: joi.Number().required().min(16),
+        back: joi.Number().required().min(3).max(4),
+        expiringDate: joi.string().required()
+    }),
+    productId: joi.string().required()
+
+})

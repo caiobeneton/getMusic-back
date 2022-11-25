795959a408bca4ece4f68f75af9161c743f7c5fb
Caio Beneton <caio.beneton@gmail.com>
2022-11-25 19:47:35 -0300

    fix: import js

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -2,8 +2,8 @@ import joi from "joi"
 
 export const productSchema = joi.object({
     name: joi.string().required().min(8),
-    price: joi.Number().required(),
+    price: joi.number().required(),
     category: joi.string().required().min(4),
     description: joi.string().required(),
-    amount: joi.Number().required()
+    amount: joi.number().required()
 })

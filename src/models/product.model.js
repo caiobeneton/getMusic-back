7862ffbf5671802d521c37e5fb4fcd7f1b2e3c7f
Caio Beneton <caio.beneton@gmail.com>
2022-11-28 11:13:10 -0300

    fix: product model

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -4,7 +4,7 @@ export const productSchema = joi.object({
     name: joi.string().required().min(3),
     price: joi.number().required(),
     category: joi.string().required().min(4),
-    imagem: joi.string().required(),
+    image: joi.string().required(),
     description: joi.string().required(),
     amount: joi.number().required()
 })
